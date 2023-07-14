4f50d4059d93aa8d1b993dd857681d7c4cb414b6
LODNight <khuunhattin123@gmail.com>
2023-07-15 01:36:28 +0700

    Done Crud Service

--- Providence/Service/Implement/AccountCRUD.cs
+++ Providence/Service/Implement/AccountCRUD.cs
@@ -47,9 +47,43 @@ namespace Providence.Service.Implement
             }
         }
 
-        public dynamic Get(int id) => _databaseContext.Accounts.FirstOrDefault(a => a.AccountId == id);
+       public dynamic Get(int id) => _databaseContext.Accounts.Where(acc => acc.AccountId == id).Select(acc => new
+        {
+            accountId = acc.AccountId,
+            firstname = acc.Firstname,
+            lastname = acc.Lastname,
+            email = acc.Email,
+            password = acc.Password,
+            phoneNumber = acc.Phone,
+            gender = acc.Gender,
+            address = acc.Address.RoadName,
+            avatar = acc.Avatar,
+            roleid = acc.RoleId,
+            rolename = acc.Role.RoleName,
+            status = acc.Status,
+            securitycode = acc.SecurityCode,
+            createdAt = acc.CreatedAt,
+            updatedAt = acc.UpdatedAt,
+        }).FirstOrDefault()!;
 
-        public dynamic Read() => _databaseContext.Accounts.ToList();
+        public dynamic Read() => _databaseContext.Accounts.Select(acc => new
+        {
+            accountId = acc.AccountId,
+            firstname = acc.Firstname,
+            lastname = acc.Lastname,
+            email = acc.Email,
+            password = acc.Password,
+            phoneNumber = acc.Phone,
+            gender = acc.Gender,
+            address = acc.Address.RoadName,
+            avatar = acc.Avatar,
+            roleid = acc.RoleId,
+            rolename = acc.Role.RoleName,
+            status = acc.Status,
+            securitycode = acc.SecurityCode,
+            createdAt = acc.CreatedAt,
+            updatedAt = acc.UpdatedAt,
+        }).ToList();
 
         public bool Update(Account entity)
         {
